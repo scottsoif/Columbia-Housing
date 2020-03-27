a848be2a21bc04d40c2e43bec3eaed1533ad61f1
Scott Soifer <soifer00@gmail.com>
2020-03-27 14:02:43 -0400

    Made it easier for selecting which groups you want

--- Housing_parser.py
+++ Housing_parser.py
@@ -14,14 +14,14 @@ def getCol(arr, idx): # returns one-dim array of any column
     return [i[idx] for i in arr]
 
 
-def getHousStats(eight=True, nine=False, ten=False, points=20):
+def getHousStats(years=(8), points=20):
     # usage: getHousStats() will return groups of 8 members before you
-    #        getHousStats(0,1,1, 20) returns groups of 9 or 10 members
-    #        getHousStats(0,1,0, 20) groups with only 9 members and so on
+    #        getHousStats((9,10), 20) returns groups of 9 or 10 members
+    #        getHousStats((9), 20) groups with only 9 members and so on
     #        points variable shows only groups >= to that value
     tally = 0;
     for g in housing[:h_idx]:  # g stands for group
-        if g[0]>=points and (g[1]==8 and eight) or (g[1]==9 and nine) or (g[1]==10 and ten):
+        if g[0]>=points and g[1] in years:
             print(g)
             tally+=1
     print("\n{}  groups before you".format(tally) )
@@ -34,6 +34,6 @@ if __name__ == '__main__':
     housing_number = 2266   # input your housing number
     h_idx = getCol(housing, 2).index(housing_number)  # our housing number index
 
-    getHousStats(1,0,0,30)  # same as getHousStats()
+    getHousStats((8,9,10),20)  # same as getHousStats()
 
 
